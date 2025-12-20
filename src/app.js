d8c3806d551ff6fb5b2581c07b1cf155c76c1654
Anas Maroof <anas-maroof@users.noreply.github.com>
2025-12-21 01:09:16 +0530

    Writing auth-middleware

--- src/app.js
+++ src/app.js
@@ -1,25 +1,47 @@
 const express = require('express');
 const app = express();
 
+const { adminAuth, userAuth } = require("./middlewares/auth");
+
+app.use("/admin", adminAuth);
+
+app.get("/user/login", (req, res) => {
+    res.send("User logged in successfully");
+})
+app.get("/user/data", userAuth, (req, res) => {
+    console.log("Inside user Middle Ware");
+    res.send("User data send!!");
+})
+
+app.get("/admin/getData", (req, res) => {
+    // Logic for sending data to database
+    res.send("All data send successfully");
+})
+
+app.get("/admin/deleteUser", (req, res) => {
+    // Logic for deleting a User from database
+    res.send("User deleted successfully");
+})
+
 // This will only handle GET call to /user
 // app.get("/user", (req, res) => {
 //     res.send({ firstName: "Anas", lastName: "Maroof" })
 // })
-app.get("/user/:userId/:name/:password", (req, res) => {
-    console.log(req.params);
-    res.send({ firstname: "Anas", lastname: "Maroof" });
-})
-app.post("/user", (req, res) => {
-    // Write logic for saving data in database
-    res.send("Data Saved in database successfully")
-})
-app.delete("/user", (req, res) => {
-    // Write logic for deleting user from database
-    res.send("User got deleted from database")
-})
-app.use("/test", (req, res) => {
-    res.send("This is test page of app");
-})
+// app.get("/user/:userId/:name/:password", (req, res) => {
+//     console.log(req.params);
+//     res.send({ firstname: "Anas", lastname: "Maroof" });
+// })
+// app.post("/user", (req, res) => {
+//     // Write logic for saving data in database
+//     res.send("Data Saved in database successfully")
+// })
+// app.delete("/user", (req, res) => {
+//     // Write logic for deleting user from database
+//     res.send("User got deleted from database")
+// })
+// app.use("/test", (req, res) => {
+//     res.send("This is test page of app");
+// })
 
 app.listen(7777, () => {
     console.log("Server is successfully learning on port 7777...");
---
+++ src/middlewares/auth.js
@@ -0,0 +1,27 @@
+const adminAuth = (req, res, next) => {
+    console.log("Admin Authorization is getting checked");
+    const token = "xyz";
+    const isAdminAuthorized = token === "xyz";
+    if (!isAdminAuthorized) {
+        res.status(401).send("Unauthorized Request");
+    }
+    else {
+        next();
+    }
+}
+const userAuth = (req, res, next) => {
+    console.log("User Authorization is getting checked");
+    const token = "xyz";
+    const isAdminAuthorized = token === "xyz";
+    if (!isAdminAuthorized) {
+        res.status(401).send("Unauthorized Request");
+    }
+    else {
+        next();
+    }
+}
+
+module.exports = {
+    adminAuth,
+    userAuth,
+};
