a4b899adca518bff346f440240789f632b4717ac
Anas Maroof <anas-maroof@users.noreply.github.com>
2025-12-22 20:26:26 +0530

    delete user API created

--- src/app.js
+++ src/app.js
@@ -42,6 +42,17 @@ app.get("/feed", async (req, res) => {
         res.status(400).send("Something went wrong");
     }
 })
+
+app.delete("/user", async(req,res)=>{
+    const userId = req.body.userId;
+    try{
+        const user = await User.findByIdAndDelete(userId);
+        res.send("User deleted successfully");
+    }
+    catch (err) {
+        res.status(400).send("Something went wrong");
+    }
+})
 connectDB()
     .then(() => {
         console.log("Database connected successfully");
