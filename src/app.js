ff3dc3d504ff96ff7870cda5dfe011049afc2fa5
Anas Maroof <anas-maroof@users.noreply.github.com>
2025-12-22 16:44:40 +0530

    dynamic data in signup API

--- src/app.js
+++ src/app.js
@@ -3,15 +3,11 @@ const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
 
+app.use(express.json());
+
 app.post("/signup", async (req, res) => {
-    const user = new User({
-        firstName: "Shah Rukh",
-        lastName: "Khan",
-        emailId: "srk@mannat.com",
-        password: "SRK@56",
-        age: 60,
-        gender: "Male",
-    })
+    console.log(req.body);
+    const user = new User(req.body);
     try {
         await user.save();
         res.send("User Added successfully");
