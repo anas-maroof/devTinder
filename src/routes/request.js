dc9bb80a59ec1b60e2597ebd6b209f35f9e415af
Anas Maroof <anas-maroof@users.noreply.github.com>
2026-01-06 17:51:12 +0530

    /request/review/:status/:requestId created

--- src/routes/request.js
+++ src/routes/request.js
@@ -53,4 +53,32 @@ requestRouter.post("/request/send/:status/:toUserId", userAuth, async (req, res)
     }
 })
 
+requestRouter.post("/request/review/:status/:requestId", userAuth, async (req, res) => {
+    try {
+        const loggedInUser = req.user;
+        const { status, requestId } = req.params;
+
+        const allowedStatus = ["accepted", "rejected"];
+        if (!allowedStatus.includes(status)) {
+            throw new Error(`${status} is not allowed. Invalid status type.`);
+        }
+
+        const connectionRequest = await ConnectionRequest.findOne({
+            _id: requestId,
+            toUserId: loggedInUser._id,
+            status: "interested"
+        });
+        if (!connectionRequest) {
+            throw new Error("Connection request not found!");
+        }
+
+        connectionRequest.status = status;
+        const data = await connectionRequest.save();
+        res.json({ message: `Connection Request ${status}`, data });
+    }
+    catch (err) {
+        res.status(400).send("ERROR : " + err.message);
+    }
+})
+
 module.exports = requestRouter;
